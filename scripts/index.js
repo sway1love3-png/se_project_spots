cf979af757e65cb539340ff7afd4ffa250e145a9
Tyesha Brown <sway1love.3@gmail.com>
2026-06-07 00:09:48 -0700

    Spots Project, Stage 5

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,28 @@
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input",
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input",
+);
 
 const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
+const newPostForm = newPostModal.querySelector(".modal__form");
+const cardImageInput = newPostModal.querySelector("#card-image-input");
+const cardCaptionInput = newPostModal.querySelector("#card-caption-input");
+
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
+const cardsList = document.querySelector(".cards__list");
 
 editProfileBtn.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
   editProfileModal.classList.add("modal_is-opened");
 });
 
@@ -21,3 +37,45 @@ newPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleEditProfileSubmit(evt) {
+  evt.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+  editProfileModal.classList.remove("modal_is-opened");
+}
+
+function createCard(imageUrl, caption) {
+  const card = document.createElement("li");
+  card.classList.add("card");
+
+  card.innerHTML = `
+    <img
+      src="${imageUrl}"
+      alt="${caption}"
+      class="card__image"
+    />
+    <div class="card__content">
+      <h2 class="card__title">${caption}</h2>
+      <button type="button" class="card__like-btn"></button>
+    </div>
+  `;
+
+  return card;
+}
+
+function handleNewPostSubmit(evt) {
+  evt.preventDefault();
+
+  const cardElement = createCard(cardImageInput.value, cardCaptionInput.value);
+
+  cardsList.prepend(cardElement);
+
+  newPostForm.reset();
+
+  newPostModal.classList.remove("modal_is-opened");
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
+
+newPostForm.addEventListener("submit", handleNewPostSubmit);
